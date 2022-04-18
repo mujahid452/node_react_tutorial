1690af55ec73500914b96f331be100ac75254c56
mujahid452 <mujeeprogrammer@gmail.com>
2022-04-18 16:46:21 +0500

    first commit

--- backend/index.js
+++ backend/index.js
@@ -1,13 +1,24 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const routesHandler = require('./routes/handler.js');
+const mongoose = require('mongoose');
+require('dotenv/config');
 
 const app = express();
 app.use(bodyParser.urlencoded({extended:false}));
 app.use(bodyParser.json());
 app.use('/', routesHandler);
+mongoose.connection.close();
 
-const PORT = 4000; // backend routing port
+mongoose.connect('mongodb+srv://mujahid:mujee452@cluster0.ei15p.mongodb.net/ToDoListDb?retryWrites=true&w=majority' ,{ useNewUrlParser: true, useUnifiedTopology: true} )
+.then ( () => {
+    console.log('Db Connected');
+})
+.catch( (err) => {
+    console.log(err);
+})
+ 
+const PORT = process.env.PORT || 4000; // backend routing port
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}.`);
 });
